52c8beea2bba53492dac8d9323a8811fced4a0ce
sdujyf <2597077223jyf@gmail.com>
2025-05-12 11:34:18 +0800

    kc

--- detection.sh
+++ detection.sh
@@ -74,7 +74,25 @@ python inference1_d.py \
     --eval_type openqa \
     --ty temporal\
     --oringin f
+python inference1_d.py \
+    --test_dataset "people_knowledge.json" \
+    --dataset_name people_knowledge \
+    --meta_save_path detection \
+    --model_name "$MODEL_NAME" \
+    --conflict_type ie \
+    --eval_type openqa \
+    --ty location\
+    --oringin f
 
+python inference1_d.py \
+    --test_dataset "people_knowledge.json" \
+    --dataset_name people_knowledge \
+    --meta_save_path detection \
+    --model_name "$MODEL_NAME" \
+    --conflict_type ie \
+    --eval_type openqa \
+    --ty Carrer\
+    --oringin f
 
 python inference1_d.py \
     --test_dataset "logo_knowledge.json" \
@@ -84,6 +102,26 @@ python inference1_d.py \
     --conflict_type ie \
     --eval_type openqa \
     --ty time\
+    --oringin f
+
+python inference1_d.py \
+    --test_dataset "logo_knowledge.json" \
+    --dataset_name logo_knowledge \
+    --meta_save_path detection \
+    --model_name "$MODEL_NAME" \
+    --conflict_type ie \
+    --eval_type openqa \
+    --ty creator\
+    --oringin f
+
+python inference1_d.py \
+    --test_dataset "logo_knowledge.json" \
+    --dataset_name logo_knowledge \
+    --meta_save_path detection \
+    --model_name "$MODEL_NAME" \
+    --conflict_type ie \
+    --eval_type openqa \
+    --ty content\
     --oringin f
 
 
