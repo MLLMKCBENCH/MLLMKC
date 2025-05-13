2d144762b23b1e0cfd9f5fc04127ba001d757473
sdujyf <2597077223jyf@gmail.com>
2025-05-14 00:49:37 +0800

    d

--- detection_xin.sh
+++ detection_xin.sh
@@ -2,7 +2,7 @@ MODEL_NAME="llava_next_llama3"
 python inference1_d_x.py \
     --test_dataset "people_knowledge.json" \
     --dataset_name people_knowledge \
-    --meta_save_path detection \
+    --meta_save_path detection_x \
     --model_name "$MODEL_NAME" \
     --conflict_type ie \
     --eval_type openqa \
@@ -12,7 +12,7 @@ python inference1_d_x.py \
 python inference1_d_x.py \
     --test_dataset "people_knowledge.json" \
     --dataset_name people_knowledge \
-    --meta_save_path detection \
+    --meta_save_path detection_x \
     --model_name "$MODEL_NAME" \
     --conflict_type ie \
     --eval_type openqa \
@@ -22,7 +22,7 @@ python inference1_d_x.py \
 python inference1_d_x.py \
     --test_dataset "people_knowledge.json" \
     --dataset_name people_knowledge \
-    --meta_save_path detection \
+    --meta_save_path detection_x \
     --model_name "$MODEL_NAME" \
     --conflict_type ie \
     --eval_type openqa \
@@ -32,7 +32,7 @@ python inference1_d_x.py \
 python inference1_d_x.py \
     --test_dataset "logo_knowledge.json" \
     --dataset_name logo_knowledge \
-    --meta_save_path detection \
+    --meta_save_path detection_x \
     --model_name "$MODEL_NAME" \
     --conflict_type ie \
     --eval_type openqa \
@@ -42,7 +42,7 @@ python inference1_d_x.py \
 python inference1_d_x.py \
     --test_dataset "logo_knowledge.json" \
     --dataset_name logo_knowledge \
-    --meta_save_path detection \
+    --meta_save_path detection_x \
     --model_name "$MODEL_NAME" \
     --conflict_type ie \
     --eval_type openqa \
@@ -52,7 +52,7 @@ python inference1_d_x.py \
 python inference1_d_x.py \
     --test_dataset "logo_knowledge.json" \
     --dataset_name logo_knowledge \
-    --meta_save_path detection \
+    --meta_save_path detection_x \
     --model_name "$MODEL_NAME" \
     --conflict_type ie \
     --eval_type openqa \
